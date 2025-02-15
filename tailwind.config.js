4ebdb7fe966665495069bb15ec52322128a70b80
NIKITA <sinrulyu@gmail.com>
2025-02-15 15:34:47 +0800

    feature: basic color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+
+				},
+				secondary: {
+
+				},
+				background: {
+					light: '#ECECEC'
+				},
+				accent: {
+					1 : '#FF6C30',
+					2: '#AC9360',
+					3: '#45D62C',
+					4: '#FFBB00',
+					5: '#CC0733',
+					6: '#BB8B42',
+					7: '#9E7AD2',
+					8: '#88898A',
+					9: '#20A2FF',
+					10: '#F67133',
+					11: '#914451'
+				}
+			},
+		},
 	},
 	plugins: [],
 };
